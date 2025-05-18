8108739d2cac58cf4222602b7103433da31a4f06
Zach Winter <zach@Mac.attlocal.net>
2025-05-18 01:56:09 -0400

    Deploy.

--- server/base.js
+++ server/base.js
@@ -1,28 +1,43 @@
-import express from 'express';
-import compression from 'compression';
-import morgan from 'morgan';
-import cors from 'cors';
-import fallback from 'express-history-api-fallback';
-export * from './util/index.js';
-import { parseConfig, connect, buildModels, buildRoutes } from './util/index.js';
-import SpotifyOauth from './plugins/spotify.js';
-import StripePayments from './plugins/stripe.js';
-import OTP from './plugins/twilio.js';
-import S3 from './plugins/s3.js';
-import Audius from './plugins/audius.js';
-import Auth from './plugins/auth.js';
+import express from "express";
+import compression from "compression";
+import morgan from "morgan";
+import cors from "cors";
+import fallback from "express-history-api-fallback";
+export * from "./util/index.js";
+import {
+  parseConfig,
+  connect,
+  buildModels,
+  buildRoutes,
+} from "./util/index.js";
+import SpotifyOauth from "./plugins/spotify.js";
+import StripePayments from "./plugins/stripe.js";
+import OTP from "./plugins/twilio.js";
+import S3 from "./plugins/s3.js";
+import Audius from "./plugins/audius.js";
+import Auth from "./plugins/auth.js";
 
 const PLUGINS = {
-  '@auth': Auth,
-  '@spotify': SpotifyOauth,
-  '@stripe': StripePayments,
-  '@s3': S3,
-  '@twilio': OTP,
-  '@audius': Audius,
+  "@auth": Auth,
+  "@spotify": SpotifyOauth,
+  "@stripe": StripePayments,
+  "@s3": S3,
+  "@twilio": OTP,
+  "@audius": Audius,
 };
 
 export async function createApp(configuration = {}) {
-  const { env, apiRoot, staticDirectory, trustProxy, httpsRedirect, middleware, schemas, plugins, register } = parseConfig(configuration || {});
+  const {
+    env,
+    apiRoot,
+    staticDirectory,
+    trustProxy,
+    httpsRedirect,
+    middleware,
+    schemas,
+    plugins,
+    register,
+  } = parseConfig(configuration || {});
 
   const app = express();
 
@@ -30,10 +45,23 @@ export async function createApp(configuration = {}) {
 
   const models = schemas ? buildModels({ schemas }) : null;
 
-  if (env.NODE_ENV === 'production') {
-    app.set('trust proxy', trustProxy);
+  app.use((req, res, next) => {
+    const host = req.hostname;
+
+    // Skip for localhost and IP addresses
+    if (host === "localhost" || /^(\d{1,3}\.){3}\d{1,3}$/.test(host)) {
+      return next();
+    }
+  });
+  
+  if (env.NODE_ENV === "production") {
+    app.set("trust proxy", trustProxy);
     app.use((req, res, next) => {
-      if (httpsRedirect && req.headers.host.protocol !== 'https' && req.url.indexOf('localhost') === -1) {
+      if (
+        httpsRedirect &&
+        req.headers.host.protocol !== "https" &&
+        req.url.indexOf("localhost") === -1
+      ) {
         return res.redirect(301, `https://${req.headers.host}${req.url}`);
       }
 
@@ -46,27 +74,29 @@ export async function createApp(configuration = {}) {
   app.use(express.json(middleware.json));
   app.use(cors(middleware.cors));
 
-  const routes = schemas ? buildRoutes({ app, schemas, apiRoot, plugins }) : null;
+  const routes = schemas
+    ? buildRoutes({ app, schemas, apiRoot, plugins })
+    : null;
 
-  Object.keys(plugins).forEach(key => {
+  Object.keys(plugins).forEach((key) => {
     const config = plugins[key];
     PLUGINS[key](app, { ...config, env, models });
   });
 
-  register?.(app, { models })
+  register?.(app, { models });
 
-  if (env.NODE_ENV === 'production') {
+  if (env.NODE_ENV === "production") {
     app.use(express.static(staticDirectory));
-    app.use(fallback('index.html', { root: staticDirectory }));
+    app.use(fallback("index.html", { root: staticDirectory }));
   }
 
-  const port = env.PORT || 2223
+  const port = env.PORT || 2223;
 
-  app.listen(port, () => `Listening on port ${port}.`)
+  app.listen(port, () => `Listening on port ${port}.`);
 
   return {
     app,
     models,
-    routes
+    routes,
   };
 }
